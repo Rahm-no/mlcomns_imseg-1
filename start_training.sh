44bebf02ed546e22f16b6c191df50370ceaaff95
RuoyuDeng <rockdenggg@gmail.com>
2022-08-19 18:58:25 -0400

    add preprocess_data_path as argument

--- start_training.sh
+++ start_training.sh
@@ -2,15 +2,14 @@
 
 num_gpus=${1:-4}
 exp_name=$2
+data_path=$3
 workload_dir="/dl-bench/ruoyudeng/mlcomns_imseg"
 
+# we can change -v /data/kits19/preprocessed_data/
 docker run --ipc=host --name=training -it --rm --runtime=nvidia \
 	-v /data/kits19/data/:/raw_data \
-	-v /data/kits19/preprocessed_data/:/data \
-	# new store results path 
-	-v ${workload_dir}/results/${exp_name}/results:/results \ 
-	# -v ${workload_dir}/results:/results \ 
-	# new store ckpts path
+	-v ${data_path}:/data \
+	-v ${workload_dir}/results/${exp_name}/results:/results \
 	-v ${workload_dir}/ckpts/${exp_name}/ckpts:/ckpts \
-	# -v ${workload_dir}/ckpts:/ckpts \ 
 	unet3d:latest /bin/bash run_and_time.sh 1 $num_gpus
+
