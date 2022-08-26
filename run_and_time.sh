788f3903dc44065d4a1d108f82a380bb1be4e62d
RuoyuDeng <rockdenggg@gmail.com>
2022-08-25 22:57:20 -0400

    trace 1 experiment params

--- run_and_time.sh
+++ run_and_time.sh
@@ -19,14 +19,14 @@ SAVE_CKPT_PATH="/ckpts"
 ##### params to be changed: ######
 
 # data loading speed (depends on cpu resources)
-NUM_WORKERS=1 
+NUM_WORKERS=0 # can not plot correctly with NUM_WORKERS>0, must stay UNCHANGED!!
 
 # evaluation frequency (I/O to disk)
-START_EVAL_AT=10 
-EVALUATE_EVERY=2
+START_EVAL_AT=15 
+EVALUATE_EVERY=4
 
 # training time and warm up time
-MAX_EPOCHS=20
+MAX_EPOCHS=40
 LR_WARMUP_EPOCHS=5
 
 if [ -d ${DATASET_DIR} ]
