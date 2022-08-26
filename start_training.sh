5bba6f3f9bdee23b11ec4b69e61d4a9dbe065793
RuoyuDeng <rockdenggg@gmail.com>
2022-08-26 15:16:09 -0400

    comment update

--- start_training.sh
+++ start_training.sh
@@ -5,17 +5,15 @@ exp_name=$2
 data_path=$3
 workload_dir="/dl-bench/ruoyudeng/mlcomns_imseg"
 
-# we can change -v /data/kits19/preprocessed_data/
-
-# sudo docker build -t unet3d:trace1 .
 
 # this is just running docker images, but we need to remount everything using `docker build` as an update to old image before we run it again
 
+# sudo docker build -t unet3d:latest .
 # run image unet3d:trace1 with the trace1 tunned params
 docker run --ipc=host --name=training -it --rm --runtime=nvidia \
 	-v /data/kits19/data/:/raw_data \
 	-v ${data_path}:/data \
 	-v ${workload_dir}/results/${exp_name}/results:/results \
 	-v ${workload_dir}/ckpts/${exp_name}/ckpts:/ckpts \
-	unet3d:trace1 /bin/bash run_and_time.sh 1 $num_gpus
+	unet3d:latest /bin/bash run_and_time.sh 1 $num_gpus
 
