4b7969872f5c9d705c093edcf353c57a1fcc1d85
jovonho <loic.hovon@gmail.com>
2023-02-21 22:36:45 -0500

    fixed

--- start_training.sh
+++ start_training.sh
@@ -9,7 +9,7 @@ mkdir -p /raid/data/imseg/run_output
 
 NUM_GPUS=${1:-8}
 CONTAINER_NAME=${2:train_imseg} 
-LOGGING_DIR=${3:"$SCRIPT_DIR/output"}
+LOGGING_DIR=${3:-"$SCRIPT_DIR/output"}
 BATCH_SIZE=${4:-4}
 DOCKER_IMAGE=${5:-"unet3d:loic"}
 NUM_WORKERS=${6:-8}
@@ -26,6 +26,6 @@ fi
 docker run --ipc=host --name=$CONTAINER_NAME -it --rm --runtime=nvidia $DOCKER_MEMORY_PARAM \
 	-v /raid/data/imseg/raw-data/kits19/data/:/raw_data \
 	-v /raid/data/imseg/29gb-npy/:/data \
-	-v $LOGGING_DIR:/results \
+	-v ${LOGGING_DIR}:/results \
 	-v /raid/data/imseg/run_output:/ckpts \
 	$DOCKER_IMAGE /bin/bash run_and_time.sh 1 $NUM_GPUS $BATCH_SIZE $NUM_WORKERS $NUM_EPOCHS 
