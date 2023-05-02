9960b2a6561e47eb899dc0f5120e6954125ac830
jovonho <loic.hovon@gmail.com>
2023-05-02 16:26:33 -0400

    cleanup, make scripts general

--- start_training_on_generated.sh
+++ start_training_on_generated.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )" )
-mkdir -p /raid/data/imseg/run_output
 
-NUM_GPUS=${1:-4}
-CONTAINER_NAME=${2:train_imseg}
-BATCH_SIZE=${3:-2}
-DOCKER_IMAGE=${4:-"unet3d:loic"}
-NUM_WORKERS=${5:-8}
-SKIP_STEP_7=${6:-""}
+LOGGING_DIR="$SCRIPT_DIR/output"
+CKPT_DIR="$SCRIPT_DIR/ckpts"
+DATA_DIR="$SCRIPT_DIR/data"
 
-DOCKER_MEMORY_PARAM=
 
-if [ ! -z "$DOCKER_MEMORY" ]
-then
-	DOCKER_MEMORY_PARAM="-m ${DOCKER_MEMORY}g"
-fi
+NUM_GPUS=${1:-8}
+CONTAINER_NAME=${2:train_unet3d}
+DOCKER_IMAGE=${3:-"unet3d:instrumented"}
+BATCH_SIZE=${4:-4}
+NUM_WORKERS=${5:-1}
+NUM_EPOCHS=${6:-30}
 
-docker run --ipc=host --name=$CONTAINER_NAME -it --rm --runtime=nvidia $DOCKER_MEMORY_PARAM \
-	-v /raid/data/imseg/29gb-gen:/data \
-	-v /raid/data/imseg/run_output:/results \
-	-v /raid/data/imseg/run_output:/ckpts \
-	$DOCKER_IMAGE /bin/bash run_and_time.sh 1 $NUM_GPUS $BATCH_SIZE $NUM_WORKERS $SKIP_STEP_7 
+docker run --ipc=host --name=$CONTAINER_NAME -it --rm --runtime=nvidia \
+	-v ${DATA_DIR}:/data \
+	-v ${LOGGING_DIR}:/results \
+	-v ${CKPT_DIR}:/ckpts \
+	$DOCKER_IMAGE /bin/bash run_and_time.sh 1 $NUM_GPUS $BATCH_SIZE $NUM_WORKERS $NUM_EPOCHS 
 	
