bbe37a70ee12650e72e7ce3f0073dcffe1788bbe
jovonho <loic.hovon@gmail.com>
2023-05-04 11:08:26 -0400

    update launch scripts

--- start_training_on_generated.sh
+++ start_training_on_generated.sh
@@ -2,20 +2,20 @@
 
 SCRIPT_DIR=$( dirname -- "$( readlink -f -- "$0"; )" )
 
-LOGGING_DIR="$SCRIPT_DIR/output"
-CKPT_DIR="$SCRIPT_DIR/ckpts"
-DATA_DIR="$SCRIPT_DIR/data"
+# Change these directories to your values!
+CKPT_DIR=""
+GENERATED_DATA_DIR=""
+
 
 NUM_GPUS=${1:-8}
 CONTAINER_NAME=${2:train_unet3d}
-DOCKER_IMAGE=${3:-"unet3d:original"}
-BATCH_SIZE=${4:-4}
-NUM_WORKERS=${5:-1}
+LOGGING_DIR=${3:-"$SCRIPT_DIR/output"}
+DOCKER_IMAGE=${4:-"unet3d:instrumented"}
+BATCH_SIZE=${5:-4}
 NUM_EPOCHS=${6:-30}
 
 docker run --ipc=host --name=$CONTAINER_NAME -it --rm --runtime=nvidia \
-	-v ${DATA_DIR}:/data \
+	-v ${GENERATED_DATA_DIR}:/data \
 	-v ${LOGGING_DIR}:/results \
 	-v ${CKPT_DIR}:/ckpts \
-	$DOCKER_IMAGE /bin/bash run_and_time.sh 1 $NUM_GPUS $BATCH_SIZE $NUM_WORKERS $NUM_EPOCHS 
-	
+	$DOCKER_IMAGE /bin/bash run_and_time.sh 1 $NUM_GPUS $BATCH_SIZE $NUM_EPOCHS 
